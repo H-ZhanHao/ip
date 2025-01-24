0b6e6ef5d54d9dc476a071610548068ae6e119e5
Zhan Hao <hoezhanhao9@gmail.com>
2025-01-24 13:36:48 +0800

    Change Duke to Eelyon

---
+++ src/main/java/Eelyon.java
@@ -0,0 +1,7 @@
+public class Eelyon {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line + "Hello! I'm Eelyon\n" + "What can I do for you?\n"
+                + line + "Bye. Hope to see you soon!\n" + line );
+    }
+}
