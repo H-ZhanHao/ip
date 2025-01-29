48159c17a9c1b0b77dbe143882da95aced8e9998
Zhan Hao <zhanhaoh@u.nus.edu>
2025-01-29 13:30:03 +0800

    Update Eelyon to add echo feature

--- src/main/java/Eelyon.java
+++ src/main/java/Eelyon.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
+
 public class Eelyon {
     public static void main(String[] args) {
+        boolean exit = false;
+        String input;
+        Scanner in = new Scanner(System.in);
+
         String line = "____________________________________________________________\n";
-        System.out.println(line + "Hello! I'm Eelyon\n" + "What can I do for you?\n"
-                + line + "Bye. Hope to see you soon!\n" + line );
+        System.out.println(line + "Hello! I'm Eelyon\n" + "What can I do for you?\n" + line);
+
+        while (!exit) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                exit = true;
+            } else {
+                System.out.println(line + input + "\nlist" + line);
+            }
+        }
+
+        System.out.println(line + "Bye. Hope to see you soon!\n" + line);
     }
+
+
 }
+
