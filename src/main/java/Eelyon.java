1dbccf2fcd64a810c40166cef642616a08445db9
Zhan Hao <zhanhaoh@u.nus.edu>
2025-01-29 14:27:58 +0800

    Update Eelyon: Add ability to store text into list

--- src/main/java/Eelyon.java
+++ src/main/java/Eelyon.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 
 public class Eelyon {
+
     public static void main(String[] args) {
         boolean exit = false;
         String input;
+        String[] list = new String[100];
+        int listIndex = 0;
         Scanner in = new Scanner(System.in);
 
         String line = "____________________________________________________________\n";
@@ -14,8 +17,16 @@ public class Eelyon {
             input = in.nextLine();
             if (input.equals("bye")) {
                 exit = true;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
+                System.out.println(line);
             } else {
-                System.out.println(line + input + "\nlist" + line);
+                list[listIndex] = input;
+                listIndex++;
+                System.out.println(line + "added: " + input + "\n" + line);
             }
         }
 
